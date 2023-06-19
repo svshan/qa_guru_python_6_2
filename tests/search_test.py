81042fb5b7d83cc25084ed1dbd58edcc9ff6c21e
Sviatlana Shandrokha <testemail12091@gmail.com>
2023-06-19 16:10:37 +0200

    comments to the test results added

--- tests/search_test.py
+++ tests/search_test.py
@@ -9,14 +9,16 @@ def browser_size():
     browser.config.window_height = 1080
 
 
-def test_successful_search(browser_size):
+def test_search_with_valid_values(browser_size):
     browser.open('https://duckduckgo.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="r1-0"] .EKtkFWMYpwzMKOYr0GYm').should(
         have.text('User-oriented Web UI browser tests in Python'))
+    print("List of results for request with valid values")
 
 
 def test_search_with_invalid_values(browser_size):
     browser.open('https://duckduckgo.com')
     browser.element('[name="q"]').should(be.blank).type('"cats and dogs dgvgjnnnfЕРВЫuhИСТИ"').press_enter()
     browser.element('.deB7ubqmD22Y2q4Hxlgr').should(have.text('No results found for'))
+    print("No results for request with invalid values")
